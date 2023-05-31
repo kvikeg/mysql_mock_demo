b2a7fe82ff909cef7056fd89889dc3f995a6b969
Philip Derbeko <philip.derbeko@gmail.com>
2023-05-31 23:06:03 +0300

    - prepare_db is working (run: node demo_service prepare) - added dependencies to package.json and package-lock.json - added .gitignore for node_modules

--- demo_service.js
+++ demo_service.js
@@ -23,13 +23,37 @@ function connect_db() {
 async function prepare_db() {
     console.log("Preparing DB");
     var dbcon = connect_db()
-
     try {
-        var res = await dbcon.query("CREATE DATABASE testdb");
-        console.log(res);
-    } catch (err) {
-        console.error("Failed:" + err);
-        return;
+        
+        try {
+            var res = await dbcon.query("CREATE DATABASE testdb;");
+            console.log(res);
+        } catch (err) {
+            var strerr = err.toString();
+            if (strerr.indexOf("ER_DB_CREATE_EXISTS") !== -1) {
+                console.log("The database already exists");
+            } else {
+                console.error("Failed:" + err);
+                return;
+            }
+        }
+
+        try {
+            var res = await dbcon.query("use testdb;");
+            console.log(res);
+            var sql = "CREATE TABLE users (name VARCHAR(255), address VARCHAR(255))";
+            res = await dbcon.query(sql);
+            console.log(res);
+        } catch (err) {
+            var strerr = err.toString();
+            if (strerr.indexOf("ER_TABLE_EXISTS_ERROR") !== -1) {
+                console.log("The table already exists");
+            } else {
+                console.error("Failed:" + err);
+                return;
+            }
+        }
+    
     } finally {
         await dbcon.close();
     }
@@ -62,13 +86,18 @@ function main_demo() {
     
 }
 
-if (require.main === module) {
-    const args = process.argv.slice(2);
-    if (args.length > 0 && args[0] == "prepare") {
-        await prepare_db();
-    } else {
-        main_demo();
+async function main() {
+
+    if (require.main === module) {
+        const args = process.argv.slice(2);
+        if (args.length > 0 && args[0] == "prepare") {
+            await prepare_db();
+        } else {
+            main_demo();
+        }
     }
 }
+
+main();
 
 module.exports = {main_demo};
